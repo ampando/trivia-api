ddfd92b8e7dd45a301c3dc1d66d7dea2855fd45f
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-23 20:48:25 -0700

    night one

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
-$(document).ready(function() {
+$(function() {
+  $('#dayTrivia').on('submit', function() { 
+    
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest(); 
+      const url = `https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple`; 
 
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response); 
+        } else {
+          reject(request.response); 
+        }
+      };
+      request.open("GET", url, true); 
+      request.send(); 
+    }); 
+    
+    promise.then(function(response) {
+      const body = JSON.parse(response); 
+      $('#question').text(`${body.results[0].question}`);
+    });
+    
+  });
 }); 
