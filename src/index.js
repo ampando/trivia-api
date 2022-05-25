c6e99ba1d6586d654cec9dcb9643fa525a3ae979
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-24 20:47:35 -0700

    tuesday night

--- src/index.js
+++ src/index.js
@@ -3,30 +3,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Trivia from './trivia.js'; 
 
 
-$(function() {
-  $('#dayTrivia').on('submit', function() { 
-    
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest(); 
-      const url = `https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple`; 
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response); 
-        } else {
-          reject(request.response); 
-        }
-      };
-      request.open("GET", url, true); 
-      request.send(); 
-    }); 
+$(document).ready(function() {
+  $('#triviaParty').click(function() {  
+    let promise = Trivia.getTrivia(); 
     
     promise.then(function(response) {
       const body = JSON.parse(response); 
       $('#question').text(`${body.results[0].question}`);
+      $('.answer').text(`${body.results[0].correct_answer}`);
+      $('#incorrect1').text(`${body.results[0].incorrect_answers[0]}`);
+      $('#incorrect2').text(`${body.results[0].incorrect_answers[1]}`);
+      $('#incorrect3').text(`${body.results[0].incorrect_answers[2]}`); 
+      $('#hidden').show(); 
+      $('.start').hide(); 
+      $('.showErrors').text(""); 
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+      $('#submitAnswer').click(function(response, event) {
+        event.preventDefault();
+        const body = JSON.parse(response); 
+        console.log("yo yo"); 
+        $('#hidden').hide(); 
+        $('.showAnswer').toggle(); 
+        $('.answerDisplay').text(`${body.results[0].correct_answer}`);
+      });
     });
-    
   });
-}); 
+});
---
+++ src/trivia.js
@@ -0,0 +1,18 @@
+export default class Trivia {
+  static getTrivia() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest(); 
+      const url = `https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple`; 
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response); 
+        } else {
+          reject(request.response); 
+        }
+      };
+      request.open("GET", url, true); 
+      request.send(); 
+    }); 
+  }
+}
